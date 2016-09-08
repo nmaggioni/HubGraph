81e76046226a08c6dac6db79ea7209ced5ffb2f6
Niccolò Maggioni <nicco.maggioni@gmail.com>
2016-09-08 21:53:12 +0200

    Deprecated method removal

--- file.go
+++ file.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"log"
 	"strings"
 )
@@ -18,17 +17,4 @@ func MarshalToMemory(d3Data D3) {
 		log.Fatalf("Unable to marshal graph data to in-memory JSON: %s", err.Error())
 	}
 	D3JSON = strings.Replace(string(d3JSON), "null", "{}", -1)
-}
-
-// MarshalToFile converts a JSON object to a string and saves it in a file, specified by the `filePath` string.
-func MarshalToFile(d3Data D3, filePath string) {
-	d3JSON, err := json.MarshalIndent(d3Data, "", "  ")
-	if err != nil {
-		log.Fatalf("Unable to marshal graph data to JSON file: %s", err.Error())
-	}
-	d3JSON = []byte(strings.Replace(string(d3JSON), "null", "{}", -1))
-	err = ioutil.WriteFile(filePath, d3JSON, 0644)
-	if err != nil {
-		log.Fatalf("Unable to write marshalled JSON to file \"%s\": %s", filePath, err.Error())
-	}
 }
