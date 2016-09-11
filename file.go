2781f1af96652c5b1be8c6c0a61314607bbc1753
Giovanni Berti <dev.giovanniberti@gmail.com>
2016-09-11 11:39:21 +0200

    Reworked dashboard integration

--- file.go
+++ file.go
@@ -23,6 +23,19 @@ func MarshalD3ToMemory(d3Data D3) {
 	D3GraphData = strings.Replace(string(d3JSON), "null", "{}", -1)
 }
 
+// TODO: Docs
+func GetLastUpdateTime() string {
+	var d3Data D3
+	d3RawGraphData := strings.Replace(D3GraphData, "{}", "null", -1)
+	err := json.Unmarshal([]byte(d3RawGraphData), &d3Data)
+
+	if err != nil {
+		log.Fatalf("Unable to unmarshal d3Data in-memory JSON to data structure:%s\n%s", err.Error(), D3GraphData)
+	}
+
+	return d3Data.LastUpdate
+}
+
 // MarshalDashboardToMemory converts a JSON object to a string and saves it in-memory, in the `DashboardData` variable.
 func MarshalDashboardToMemory(dashboardData Dashboard) {
 	dashboardJSON, err := json.MarshalIndent(dashboardData, "", "  ")
